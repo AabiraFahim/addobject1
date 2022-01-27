1cf091e4c4719d620ea981b412e02688b8a497db
AabiraFahim <aabira20jul@gmail.com>
2022-01-27 21:38:01 +0500

    adding

---
+++ check/Assets/scripts/InstantiateManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InstantiateManager : MonoBehaviour
+{
+    public GameObject Prefab;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    int i;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void createobject()
+    {
+        Instantiate(Prefab, new Vector3(i * 5f, 0f, 0f), Quaternion.Euler(0f, i * 50f, 0f));
+        i++;
+        
+    }
+    public void rotateobject()
+    {
+        GameObject.Find("Bed_Prefab(Clone)").transform.Rotate(0.0f, -45f, 0.0f, Space.World);
+    }
+
+    public void cancelobject()
+    {
+        Destroy(gameObject);
+    }
+    
+    
+}
---
+++ check/Assets/scripts/rotate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.up, -10);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up, 10);
+        }
+    }
+
+    public void rotateobject()
+    {
+        
+        if (Input.GetMouseButton(0)|| Input.GetMouseButton(1)||Input.GetMouseButton(2))
+        {
+            RaycastHit raycastHit;
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.transform.name=="Bed_Prefab(Clone)")
+                {
+                    
+                        transform.Rotate(0.0f, 45f, 0.0f, Space.World);
+                    
+                }
+            }
+        }
+    }
+
+}
+    
+
